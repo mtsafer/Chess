f6529bf34945607b4110cf53781aab37574fefa0
Martin Safer <mtsafer@gmail.com>
2016-06-22 15:11:01 -0700

    touch up knight

--- lib/knight.rb
+++ lib/knight.rb
@@ -13,6 +13,5 @@ class Knight
 		moves = [ [ @x + 2, @y + 1 ], [ @x + 2, @y - 1 ], [ @x + 1, @y + 2 ],
 						  [ @x + 1, @y - 2 ], [ @x - 1, @y + 2 ], [ @x - 1, @y - 2 ],
 						  [ @x - 2, @y + 1 ], [ @x - 2, @y -1 ] ]
-		moves.map { |move| Knight.new(move, @history) }
 	end
 end
