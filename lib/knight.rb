5703f4fff7ec492a6f1da78bf31c28479cfecad7
Martin Safer <mtsafer@gmail.com>
2016-06-22 16:26:09 -0700

    generalize more functionality into token

--- lib/knight.rb
+++ lib/knight.rb
@@ -4,9 +4,9 @@ class Knight < Token
 
 	#returns an array of knight objects at each of the 8 possible spots to go
 	#note: this list does not exlude moves off the board
-	def next_moves
-		moves = [ [ @x + 2, @y + 1 ], [ @x + 2, @y - 1 ], [ @x + 1, @y + 2 ],
-						  [ @x + 1, @y - 2 ], [ @x - 1, @y + 2 ], [ @x - 1, @y - 2 ],
-						  [ @x - 2, @y + 1 ], [ @x - 2, @y - 1 ] ]
+	def next_moves board
+		moves = [ [ x + 2, y + 1 ], [ x + 2, y - 1 ], [ x + 1, y + 2 ],
+						  [ x + 1, y - 2 ], [ x - 1, y + 2 ], [ x - 1, y - 2 ],
+						  [ x - 2, y + 1 ], [ x - 2, y - 1 ] ]
 	end
 end
--- lib/token.rb
+++ lib/token.rb
@@ -1,9 +1,32 @@
 class Token
-	attr_reader :x, :y, :history, :position
+	attr_reader :history, :position
 	def initialize( position, history = [] ) # position is [x,y]
-		@x = position[0]
-		@y = position[1]
 		@position = position
 		@history = history + [ position ]
+	end
+
+	def x
+		position[0]
+	end
+
+	def y
+		position[1]
+	end
+
+	def next_moves board
+		#this method should always be overriden.
+		#it is included here to satisfy the move method.
+	end
+
+	#moves the token to the given spot if it is a legal move.
+	#returns false if it an illegal move
+	def move( move_to, board ) #the state of the board is passed
+		if( board.on_board?(move_to) #check that the move is on the board
+		&&  next_moves(board).include?(move_to) #check that it's a real move
+		&&  board.at_position(move_to).allegiance != @ally) #can't move onto ally 
+			@position = move_to #set the new position
+		else
+			false #return false if move_to can't resolve
+		end
 	end
 end
