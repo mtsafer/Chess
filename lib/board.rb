5057b8782720fb08dab5ead0b1f305749c2a3a62
Martin Safer <mtsafer@gmail.com>
2016-06-24 09:49:49 -0700

    improve #look

--- lib/board.rb
+++ lib/board.rb
@@ -23,16 +23,33 @@ class Board
 		found
 	end
 
+	#displays the current board state
 	def view
-		(0..@spots.length/8).reverse_each do |i|
+		count = 0
+		dark = "\u2591"
+		light = "\u2593"
+		shade_line_2 = "   " + (dark*6 + light*6)*4
+		shade_line_1 = "   " + (light*6 + dark*6)*4
+		base_letters = "     a     b     c     d     e     f     g     h\n\n"
+		(0...@spots.length/8).reverse_each do |i|
+			puts count % 2 == 0 ? shade_line_1 : shade_line_2
+			print (i+1).to_s + "  "
 			line = @spots[i*8...(i+1)*8]
-			line.each { |spot| print spot[1].img}
+			line.each do |spot|
+				print count % 2 == 0 ? light*2 : dark*2
+				if spot[1].img == " "
+					print count % 2 == 0 ? light*2 : dark*2
+				else
+					print spot[1].img + " "
+				end
+				print count % 2 == 0 ? light*2 : dark*2
+				count += 1
+			end
 			puts ""
+			puts count % 2 == 0 ? shade_line_1 : shade_line_2
+			count += 1
 		end
-		#spots.each do |spot|
-	#		puts "" if spot[0][0] == 0
-	#		print spot[1].img
-	#	end
+		puts "\n" + base_letters
 	end
 
 end
