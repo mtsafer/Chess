016787fed965c0c143ccf6613ed7bbfa70a384cf
Martin Safer <mtsafer@gmail.com>
2016-06-24 12:02:43 -0700

    add save/load functionality

--- run.rb
+++ run.rb
@@ -1,7 +1,58 @@
 require_relative "lib/game"
+require "yaml"
 
 private
 	
+	#loads a saved game
+	def load_game
+		print "Load game (y/n): "
+		load = gets.chomp.downcase[0]
+		return false if load == 'n'
+		return loader if load == 'y'
+		load_game if (load != 'n' || load != 'y')
+	end
+
+	#Prompts the user for a file to load
+	def loader_helper
+		if !(Dir.exists? "saves") || Dir["saves/*"].length == 0
+			puts "There are no save files...Starting game."
+			return false
+		end
+		saves = Dir.entries("saves")
+		saves.delete(".")
+		saves.delete("..")
+		puts "The save files are: #{saves.join(", ")}"
+		print "Type the save name: "
+		save = gets.chomp
+		unless saves.include? save
+			puts "That file doesn't exist. Try again.\n\n"
+			save = loader_helper 
+		end
+		save
+	end
+
+	#returns the saved object, or false if there are no saves
+	def loader
+		save = loader_helper
+		if save
+			File.open("saves/#{save}", 'r'){ |f|
+				return YAML.load(f)
+			} 
+		end
+		false
+	end
+
+	def save_game game
+		print "Name the save: "
+		save_name = gets.chomp.downcase
+		Dir.mkdir "saves" unless Dir.exists? "saves"
+		file_path = File.join("saves", "#{save_name}")
+		File.open(file_path, "w") { |f|
+			f.write(YAML.dump(game))
+		}
+		puts "The game has been saved!"
+	end
+
 	def safe_input_from game
 		print "#{game.active.name} (#{game.active.allegiance}) " +\
 					 "select a piece to move (ex: b2): "
@@ -10,6 +61,9 @@ private
 			input = input.split("")
 			input[0] = input[0].ord - 97
 			input[1] = input[1].to_i - 1
+		elsif input == "save"
+			save_game game
+			input = safe_input_from game
 		else
 			puts "\ninvalid input\n\n"
 			input = safe_input_from game
@@ -25,6 +79,9 @@ private
 			input = input.split("")
 			input[0] = input[0].ord - 97
 			input[1] = input[1].to_i - 1
+		elsif input == "save"
+			save_game game
+			input = safe_input_to game
 		else
 			puts "\ninvalid input\n\n"
 			input = safe_input_to game
@@ -39,7 +96,9 @@ private
 		move
 	end
 
-	game = Game.new
+	load = load_game
+	load ? game = load : game = Game.new
+	puts "Type save at any point to save the game"
 	game.look
 	#play the game
 	until game.winner
